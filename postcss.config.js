fae656c66e62ca6821e0dbf19197a399ce364f3a
@abdullah.muzaki <abdullah.muzaki@aruna.id>
2023-07-13 17:53:26 +0700

    sd

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const purgecss = require("@fullhuman/postcss-purgecss");
 module.exports = {
   plugins: [
     purgecss({
-      content: ["./src/**/*.html"],
+      content: ["*.html"],
     }),
   ],
 };
